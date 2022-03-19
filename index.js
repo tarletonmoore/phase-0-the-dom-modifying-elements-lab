a0fd564a755292d5918825ed1dcac3bc2ca4c6bf
John Tarleton Moore <tarletonmoore@gmail.com>
2022-03-19 14:10:58 -0400

    commit tree 674897718c94a78e8e29a5922445726ed74e8871

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove(`#main`)
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Tarleton is the champion"
+
+document.body.append(newHeader)
+
